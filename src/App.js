93499489b6b285bf30c9179e5f45a6008fd426de
michaelessiet <emsaa2002@gmail.com>
2020-10-18 22:04:20 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React , {useState} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import FormPage from './components/Form'
-import Home from './components/Home';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FormPage from "./components/Form";
+import Home from "./components/Home";
 
 const api_key = "58478525c20913e046c9ee35da9e0db7";
 
 function App() {
-
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
+  const [inputText, setinputText] = useState("");
 
   const api_call = () => {
     fetch(
@@ -20,13 +20,12 @@ function App() {
         console.log(result);
         setWeather(result);
       });
-    
-  }
+  };
 
   return (
     <div className="App">
-     <FormPage setCity = {setCity} api_call={api_call} city={city}/>
-     <Home api_call={api_call} weather={weather}/>
+      <FormPage setCity={setCity} api_call={api_call} city={city} inputText={inputText} setinputText={setinputText} />
+      <Home api_call={api_call} weather={weather} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import { Form , Button} from 'react-bootstrap';
 
-function FormPage ({setCity,city, api_call}) {
 
-  // const [inputText, setinputText] = useState("");
+function FormPage ({setCity,city, api_call, inputText, setinputText}) {
 
   const handleChange = (e) => {
     setCity(e.target.value)
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setCity(e.target.value)
-    api_call()
+    e.preventDefault();
+    setCity(e.target.value);
+    api_call();
+    console.log(city)
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <div className='form-container'>
-        <Form.Group>
-          <Form.Control placeholder="City" value={city} onChange={handleChange} />
-        </Form.Group>
-        <Form.Group>
-          
-          <Form.Control placeholder="Country" />
-        </Form.Group>
-        <Button type='submit' variant='primary' >
-          Submit
-        </Button>
-      </div>
-    </Form>
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <div className="form-container">
+          <Form.Group>
+            <Form.Control
+              placeholder="City"
+              value={city}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control placeholder="Country" />
+          </Form.Group>
+          <div className="buttons">
+            <Button className="submit" type="submit" variant="primary">
+              Submit
+            </Button>
+          </div>
+        </div>
+      </Form>
+    </div>
   );
 }
 
